6d0fa1d207ee112bdbecab885d5e567eed74511f
unknown <Joel.Figueroa002@mymdc.net>
2023-11-29 11:16:59 -0500

    added oxygentank script

---
+++ Assets/Jojo/Scripts/OxygenTank.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OxygenTank : MonoBehaviour
+{
+    //OxygenTank should only be accessible by UI to display how much Oxygen is left in the tank.
+    public float oxygenValue = 100f;
+    private float addOxygen = 15f;
+    private float removeOxygen = -25f;
+    private float oxygenLeak = 2f;
+
+
+    //This WaitForSeconds could be reused to keep a consistent starting time throughout the game.
+    public WaitForSeconds startingTime;
+
+    public Slider oxygenSlider;
+
+    void Start()
+    {
+        oxygenSlider.value = 100;
+        startingTime = new WaitForSeconds(3);
+        StartCoroutine(LoseOxygen());
+        print(oxygenLeak);
+    }
+
+    void Update()
+    {
+        oxygenSlider.value = oxygenValue;
+        if (oxygenValue <= 0)
+        {
+            //trigger player death.
+        }
+        print(oxygenValue);
+    }
+
+    private void OnCollisionEnter(Collision other) //Checks only for collision with oxygen tanks or with enemies/enemy lasers and appropriately addresses the data.
+    {
+        if (other.gameObject.tag == "oxygen")
+        {
+            oxygenValue += addOxygen;
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "EnemyLaser") oxygenValue -= removeOxygen;
+    }
+
+    IEnumerator LoseOxygen() //Timer that'll lower the Oxygen Value until it reaches zero or less.
+    {
+        yield return startingTime;
+        while (oxygenValue > 0) 
+        {
+            oxygenValue -= oxygenLeak;
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
