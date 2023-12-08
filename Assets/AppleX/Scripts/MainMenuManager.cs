c827eef0afa68750738f1339bf12bd6ded70d60f
MDCJosh <Joshandresbr25@gmail.com>
2023-12-08 11:45:08 -0500

    Main Menu Done
    Lets Keep Working Love u Guys <3

---
+++ Assets/AppleX/Scripts/MainMenuManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject mainMenuPanel;
+    public GameObject aboutPanel;
+
+
+    public void Start()
+    {
+        aboutPanel.SetActive(false);
+    }
+
+    public void OpenAbout()
+    {
+        aboutPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+
+    public void Return()
+    {
+        aboutPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
