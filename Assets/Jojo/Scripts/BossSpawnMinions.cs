1a31424771d4bdaba08ad543bb923ae9989bce55
GunzRuls <143023790+GunzRuls@users.noreply.github.com>
2023-12-09 20:05:07 -0500

    update

--- Assets/Jojo/Scripts/BossSpawnMinions.cs
+++ Assets/Jojo/Scripts/BossSpawnMinions.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 
 public class BossSpawnMinions : MonoBehaviour
 {
+    
+
+    [Header("Oxygen Info")]
+    public Transform[] oxygenSpawners;
+    public GameObject oxygenPrefab;
+    private float respawnTime = 15f;
+    private bool oxygenCooldown;
+
     [Header("Prowler Info")]
     public Transform[] prowlersSpawners;
     public GameObject prowlerPrefab;
@@ -12,4 +20,31 @@ public class BossSpawnMinions : MonoBehaviour
     [Header("Alien Info")]
     public Transform[] alienSpawners;
     public GameObject alienPrefab;
+
+    private void Start()
+    {
+        oxygenCooldown = false;
+    }
+
+    private void Update()
+    {
+        if (!oxygenCooldown)
+        {
+            StartCoroutine(SpawnOxygen());
+        }
+    }
+
+    private IEnumerator SpawnOxygen()
+    {
+        oxygenCooldown = true;
+        yield return new WaitForSeconds(respawnTime);
+        for (int i = 0; i < oxygenSpawners.Length; i++)
+        {
+            if (oxygenSpawners[i].childCount == 0)
+            {
+                Instantiate(oxygenPrefab, oxygenSpawners[i]);
+            }
+        }
+        oxygenCooldown = false;
+    }
 }
